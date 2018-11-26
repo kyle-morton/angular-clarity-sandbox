e686bdc3819917c4a3d564cfb4adf3eb8961aae5
Kyle Morton <kylemortondevelopment@gmail.com>
2018-11-25 22:28:57 -0600

    pagination working, about to do server side

--- angular-clarity-sandbox/src/app/modules/admin/components/users/users.component.ts
+++ angular-clarity-sandbox/src/app/modules/admin/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/modules/services/user.service';
 import { User } from 'src/app/models/user';
+import { ClrDatagridStateInterface } from '@clr/angular';
 
 @Component({
   selector: 'app-users',
@@ -11,13 +12,36 @@ import { User } from 'src/app/models/user';
 export class UsersComponent implements OnInit {
 
   public users : Array<User>;
+  public loading = true;
+  private page = 1;
+  private count = 10;
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.GetUsers().subscribe(result => {
+    
+    //get initial list of 10 users
+    // this.userService.GetUsers(this.page, this.count)
+    this.userService.GetUsers()
+    .subscribe(result => {
       this.users = result;
+      this.loading = false;
     });
+
   }
+
+  // refresh(state: ClrDatagridStateInterface) {
+
+  //   let from = state.page.from;
+  //   let size = state.page.size;
+
+  //   this.loading = true;
+
+  //   this.userService.GetUsers(this.page, this.count)
+  //   .subscribe(result => {
+  //     this.users = result;
+  //     this.loading = false;
+  //   });
+}
 
 }
--- angular-clarity-sandbox/src/app/modules/services/user.service.ts
+++ angular-clarity-sandbox/src/app/modules/services/user.service.ts
@@ -6,11 +6,18 @@ import { User } from 'src/app/models/user';
 @Injectable()
 export class UserService {
 
+  private apiUrl = 'http://5be5a16c48c1280013fc3da5.mockapi.io/api/Users';
+
   constructor(private api: ApiService) { }
 
-  public GetUsers() {
+  public GetUsers(page = 0, limit = 0) {
+
+    let url = this.apiUrl;
+    if (page > 0 && limit > 0)
+      url += `?page=${page}&limit=${limit}`;
+
     return this.api
-    .Get('http://5be5a16c48c1280013fc3da5.mockapi.io/api/Users')
+    .Get(url)
     .pipe(
       map(result => {
         return result as Array<User>;
