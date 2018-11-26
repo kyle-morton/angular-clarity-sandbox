710b3e844f4e1b77aa7624b3928e1d53b9df8261
Kyle Morton <kylemortondevelopment@gmail.com>
2018-11-25 19:49:17 -0600

    added nav links, need to add auth service to check user auth status

--- angular-clarity-sandbox/src/app/modules/admin/components/shell/shell.component.ts
+++ angular-clarity-sandbox/src/app/modules/admin/components/shell/shell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
@@ -9,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  LogOut() {
+    this.router.navigateByUrl('/');
   }
 
 }
